b21a5a49a21c31ba7ade419f8816eb6af514d61a
alsth712 <alsth712@naver.com>
2023-08-18 12:45:47 +0900

    fix: CopyRight 컴포넌트 url 수정

--- frontend/src/components/CopyRight.js
+++ frontend/src/components/CopyRight.js
@@ -1,11 +1,17 @@
+import { useRouter } from 'next/router';
 const { Typography } = require('@mui/material');
 import Link from '@mui/material/Link';
 
 export default function Copyright() {
+  const router = useRouter();
+  const moveHome = () => {
+    router.push('/');
+  };
+
   return (
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" onClick={moveHome}>
         Woochacha
       </Link>{' '}
       {new Date().getFullYear()}
