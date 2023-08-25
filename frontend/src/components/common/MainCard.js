aea7adeb5b9bd46887c77fa5b371c2b288b156bb
alsth712 <alsth712@naver.com>
2023-08-25 12:29:46 +0900

    feat: main card 컴포넌트 구현

---
+++ frontend/src/components/common/MainCard.js
@@ -0,0 +1,30 @@
+import { Box, Container, Typography } from '@mui/material';
+import React from 'react';
+
+export default function MainCard({ titleVal, subTitleVal, titleColor }) {
+  return (
+    <Box
+      sx={{
+        bgcolor: 'background.paper',
+        my: 10,
+        pt: 8,
+        pb: 6,
+      }}>
+      <Container maxWidth="md">
+        <Typography
+          component="h3"
+          variant="h3"
+          align="center"
+          fontWeight="bold"
+          color={titleColor}
+          mb={5}
+          gutterBottom>
+          {titleVal}
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          {subTitleVal}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
