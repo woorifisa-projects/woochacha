420f572e0a03fdc75bae5b9d6e62e1f95bab5c54
ShinChanCoding <87774238+ShinChanCoding@users.noreply.github.com>
2023-08-27 22:51:18 +0900

    1. QLDB로부터 데이터를 받아와 유저와 QLDB에 저장되어 있던 차량 소유주 의 데이터를 서로 비교하는 로직 구현 (#81)
    2. 일치하면 차량 승인 및 saleform을 저장하고 일치하지 않으면 바로 반려를 하는 로직 구현

--- backend/src/main/java/com/woochacha/backend/domain/qldb/service/serviceImpl/QldbServiceImpl.java
+++ backend/src/main/java/com/woochacha/backend/domain/qldb/service/serviceImpl/QldbServiceImpl.java
@@ -26,7 +26,7 @@ public class QldbServiceImpl implements QldbService {
 
     // QLDB에 저장된 차량 번호와 같은 차량 소유주의 이름과 전화번호를 찾아준다.
     @Override
-    public Pair<String,String> inquiryCarOwnerInfo(String carNum) {
+    public Pair<String,String> getCarOwnerInfo(String carNum) {
         try {
             qldbDriver.QldbDriver().execute(txn -> {
                 Result result = txn.execute(
@@ -50,8 +50,7 @@ public class QldbServiceImpl implements QldbService {
             qldbDriver.QldbDriver().execute(txn -> {
                 Result result = txn.execute(
                         "SELECT r_id FROM " + tableName + " AS r BY r_id WHERE r.car_num=?", ionSys.newString(carNum));
-                IonStruct carOwner = (IonStruct) result.iterator().next();
-                IonString carMetaId = (IonString) carOwner.get("r_id");
+                IonString carMetaId = (IonString) result.iterator().next();
                 metaId = carMetaId.stringValue();
             });
             return metaId;
@@ -66,11 +65,10 @@ public class QldbServiceImpl implements QldbService {
         try {
             qldbDriver.QldbDriver().execute(txn -> {
                 Result result = txn.execute(
-                        "SELECT COUNT(*) FROM car AS r WHERE r.car_num=? AND r.accident_type=? AND r.accident_desc =? AND r.metadata.id =?",
+                        "SELECT COUNT(*) FROM history(car_accident) AS r WHERE r.car_num=? AND r.accident_type=? AND r.accident_desc =? AND r.metadata.id =?",
                         ionSys.newString(carNum), ionSys.newString(accidentType), ionSys.newString(accidentDesc), ionSys.newString(metaId));
-                IonStruct countHistory = (IonStruct) result.iterator().next();
-                IonInt countHistoryInt = (IonInt) countHistory.get("_1");
-                countAccidentHistory = countHistoryInt.intValue();
+                IonInt countHistory = (IonInt) result.iterator().next();
+                countAccidentHistory = countHistory.intValue();
             });
             return countAccidentHistory;
         } catch (Exception e) {
