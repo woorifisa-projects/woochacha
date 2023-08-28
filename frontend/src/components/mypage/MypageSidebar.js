8a5a30c708cc4963702d430af5af3dcaf4ef059b
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-08-28 16:29:00 +0900

    [design] 마이페이지 UI 레이아웃 구성 (#87)
    * feat: mypage 공통 중첩 레이아웃 적용
    
    * feat: mypage 목록 공통 컴포넌트 구현
    
    * feat: mypage 프로필 컴포넌트 생성
    
    * feat: mypage 세부 기본적인 라우팅 페이지 생성
    
    * chore: 경로 이동을 위한 버튼 추가
    
    * chore: PR#83의 data 전달 형식에 맞춰서 매물리스트 dummy data 수정

---
+++ frontend/src/components/mypage/MypageSidebar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Box, Container, ListItemText, MenuItem, MenuList, Paper } from '@mui/material';
+import { HEADER_MINI_MENU } from '@/constants/string';
+import { useRouter } from 'next/router';
+
+export default function MypageSidebar() {
+  const router = useRouter();
+  const userId = 1; // DUMMY_DATA
+
+  const myPageSidebarCss = {
+    container: {
+      width: '20%',
+      height: '80%',
+      mt: 3,
+    },
+    mypageMenuPaper: {
+      backgroundColor: '#D6E6F5',
+      borderRadius: '0px',
+      minWidth: '150px',
+      maxWidth: '200px',
+    },
+    mypageMenuItem: {
+      borderRadius: '0px',
+      minWidth: '150px',
+      maxWidth: '200px',
+      paddingTop: 3,
+      paddingBottom: 3,
+      '&:hover': {
+        cursor: 'pointer',
+        backgroundColor: '#B2D8FA',
+      },
+    },
+  };
+
+  const handleMove = (url) => {
+    router.push(url);
+  };
+
+  return (
+    <Container sx={myPageSidebarCss.container}>
+      <Box>
+        <Paper sx={myPageSidebarCss.mypageMenuPaper}>
+          <MenuList dense>
+            {HEADER_MINI_MENU.CONTENTS.map((selectItem, idx) => {
+              return (
+                <MenuItem
+                  sx={myPageSidebarCss.mypageMenuItem}
+                  key={idx}
+                  onClick={() => handleMove(`${selectItem.pageUrl}/${userId}`)}>
+                  <ListItemText inset>{selectItem.pageName}</ListItemText>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </Paper>
+      </Box>
+    </Container>
+  );
+}
