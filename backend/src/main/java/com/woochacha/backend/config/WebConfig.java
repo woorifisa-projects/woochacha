b881aa1f9f58e2c313083761a44ebf24d55adf14
KimSuhyun <suhun1054@gmail.com>
2023-08-28 18:26:09 +0900

    build: webconfig allowedOrigins front-end port update

--- backend/src/main/java/com/woochacha/backend/config/WebConfig.java
+++ backend/src/main/java/com/woochacha/backend/config/WebConfig.java
@@ -7,9 +7,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000","http://localhost:5500",
-                       "https://web.postman.co/", "http://15.165.239.79:3000", "http://13.125.32.208:8080",
-                        "https://15.165.239.79:3000", "https://13.125.32.208:8080") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "http://localhost:80", "https://web.postman.co/",
+                        "http://15.165.239.79:80", "http://13.125.32.208:8080",
+                        "https://15.165.239.79:80", "https://13.125.32.208:8080") // 허용할 출처
                 .allowedMethods("GET", "POST", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
