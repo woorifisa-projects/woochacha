01cf3b4020ada201ed32c8c3bbdb99b92815a586
seungyeon gim <kjm061652@naver.com>
2023-08-28 16:42:47 +0900

    feat : 상품 수정 요청에 사용되는 Dto객체 생성

---
+++ backend/src/main/java/com/woochacha/backend/domain/mypage/dto/UpdatePriceDto.java
@@ -0,0 +1,15 @@
+package com.woochacha.backend.domain.mypage.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UpdatePriceDto {
+
+    // 유효성 처리를 위해 Dto객체 생성
+    @NotNull(message = "필수 입력 값입니다.")
+    @Min(value = 0, message = "가격은 0보다 큰 값이어야 합니다.")
+    private Integer updatePrice; // 수정할 가격
+}
