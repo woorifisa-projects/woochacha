539cc25a8475b523b64551eb602357a4486f6670
alsth712 <alsth712@naver.com>
2023-08-30 15:12:34 +0900

    feat: 공통 basic modal 구현

---
+++ frontend/src/components/common/BasicModal.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+
+export default function BasicModal(props) {
+  const [mounted, setMounted] = useState(false);
+  const { onClickModal, isOpen, modalContent, callBackFunc } = props;
+
+  const handleClickNo = () => {
+    onClickModal(false);
+  };
+
+  const handleClickYes = () => {
+    onClickModal(false);
+    callBackFunc(); // 콜백함수 실행
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    mounted && (
+      <div>
+        <Dialog
+          maxWidth="xs"
+          fullWidth
+          open={isOpen}
+          onClose={handleClickNo}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">{modalContent.modalTitle}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {modalContent.modalContents}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" onClick={handleClickNo} color="error">
+              {modalContent.modalNoBtn}
+            </Button>
+            <Button variant="contained" onClick={handleClickYes} color="primary">
+              {modalContent.modalYesBtn}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  );
+}
