651dc0937056391a751b780f4e4b4532b7002a18
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-09-01 15:19:59 +0900

    [feature] 메인페이지 백엔드 api 연동 (#125)
    * feat: filtering select box 컴포넌트 분리
    
    * feat: main page 백엔드 데이터 렌더링
    
    * chore: 게시글 card 컴포넌트 수정
    
    * chore: detail page 렌더링 관련 추가 및 수정
    
    * feat: product axios 호출 관련 기능 추가

---
+++ frontend/src/components/product/MultipleSelect.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { Box, OutlinedInput, InputLabel, MenuItem, FormControl, Select, Chip } from '@mui/material';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name, itemName, theme) {
+  return {
+    fontWeight:
+      itemName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function MultipleSelect(props) {
+  const { selectMenu, selectItems, onChangeSelect } = props;
+  const theme = useTheme();
+  const [itemName, setItemName] = useState([]);
+
+  const handleChange = (event, itemId) => {
+    const {
+      target: { value },
+    } = event;
+    setItemName(typeof value === 'string' ? value.split(',') : value);
+    onChangeSelect(event, itemId);
+  };
+
+  const multipleBoxCss = {
+    formControl: {
+      m: 1,
+      width: '15rem',
+    },
+    selectBox: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    box: { display: 'flex', flexWrap: 'wrap', gap: 0.5 },
+  };
+
+  return (
+    <FormControl sx={multipleBoxCss.formControl}>
+      <InputLabel id="demo-multiple-chip-label">{selectMenu.label}</InputLabel>
+      <Select
+        labelId="demo-multiple-chip-label"
+        id="demo-multiple-chip"
+        multiple
+        sx={multipleBoxCss.selectBox}
+        value={itemName}
+        onChange={(e) => handleChange(e, selectMenu.id)}
+        input={<OutlinedInput id="select-multiple-chip" label={selectMenu.label} />}
+        renderValue={(selected) => (
+          <Box sx={multipleBoxCss.box}>
+            {selected.map((value) => (
+              <Chip
+                key={value}
+                label={selectItems.find((item) => item.id == value)['name']}
+                variant="light"
+                color="primary"
+                size="small"
+              />
+            ))}
+          </Box>
+        )}
+        MenuProps={MenuProps}>
+        {selectItems.map((itemChip) => (
+          <MenuItem
+            key={itemChip.id}
+            value={itemChip.id}
+            style={getStyles(itemChip, itemName, theme)}>
+            {itemChip.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
--- frontend/src/pages/product/detail/[productId].js
+++ frontend/src/pages/product/detail/[productId].js
@@ -3,100 +3,34 @@ import { CssBaseline, ThemeProvider, responsiveFontSizes } from '@mui/material';
 import theme from '@/styles/theme';
 import MiniCard from '@/components/common/MiniCard';
 import { useRouter } from 'next/router';
+import { productDetailGetApi } from '@/services/productApi';
+import DetailProduct from '@/components/product/DetailProduct';
 
 export default function ProductDetail() {
   const router = useRouter();
   const { productId } = router.query;
   let responsiveFontTheme = responsiveFontSizes(theme);
   const [mounted, setMounted] = useState(false);
+  const [detailProduct, setDetailProduct] = useState();
 
-  // TODO: API 호출 후, dummy data 수정 예정
-  const dummyDetailData = {
-    productBasicInfo: {
-      title: '기아 올 뉴 카니발 2018년형',
-      carNum: '22나2222',
-      branch: '서울',
-      price: 2690,
-    },
-    productDetailInfo: {
-      capacity: 9,
-      distance: 110000,
-      carType: 'RV',
-      fuelName: '디젤',
-      transmissionName: '오토',
-      produdctAccidentInfoList: [
-        {
-          type: '침수사고',
-          count: 1,
-        },
-        {
-          type: '교통사고',
-          count: 2,
-        },
-      ],
-      productExchangeInfoList: [
-        {
-          type: '본네트',
-          count: 1,
-        },
-        {
-          type: '뒷문',
-          count: 1,
-        },
-      ],
-    },
-    productOptionInfo: [
-      {
-        option: '열선시트',
-        whether: 1,
-      },
-      {
-        option: '스마트키',
-        whether: 0,
-      },
-      {
-        option: '블랙박스',
-        whether: 1,
-      },
-      {
-        option: '네비게이션',
-        whether: 0,
-      },
-      {
-        option: '에어백',
-        whether: 1,
-      },
-      {
-        option: '썬루프',
-        whether: 1,
-      },
-      {
-        option: '하이패스',
-        whether: 0,
-      },
-      {
-        option: '후방카메라',
-        whether: 1,
-      },
-    ],
-    productOwnerInfo: null,
-    carImageList: null,
-  };
-
-  // data 불러온 이후 필터링 data에 맞게 렌더링
   useEffect(() => {
+    productId &&
+      productDetailGetApi(productId).then((data) => {
+        setDetailProduct(data);
+      });
     setMounted(true);
   }, []);
 
   return (
+    detailProduct &&
     mounted && (
       <ThemeProvider theme={responsiveFontTheme}>
         <CssBaseline />
         {/* main page */}
         <main>
           {/* 차량 디테일 페이지 */}
-          <MiniCard colorVal="#def2ff" shadowVal={3} marginVal={10}>
-            차량 {productId}의 디테일 페이지
+          <MiniCard colorVal="#FFF" shadowVal={3} marginVal={10}>
+            <DetailProduct detailItem={detailProduct} />
           </MiniCard>
         </main>
       </ThemeProvider>
