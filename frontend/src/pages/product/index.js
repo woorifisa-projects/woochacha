78dedebc743cd18f2027be091167b2a0519f2a2a
alsth712 <alsth712@naver.com>
2023-09-02 12:38:36 +0900

    chore: 구매 부분 필요없는 코드 삭제 및 수정

--- frontend/src/pages/product/index.js
+++ frontend/src/pages/product/index.js
@@ -13,107 +13,24 @@ import SearchBar from '@/components/product/SearchBar';
 import ProductCard from '@/components/product/ProductCard';
 import FilterSideBar from '@/components/product/FilterSideBar';
 import { useEffect, useState } from 'react';
+import { allProductGetApi } from '@/services/productApi';
+import { useRouter } from 'next/router';
 
 export default function Products() {
   const [mounted, setMounted] = useState(false);
+  const [allProducts, setAllProducts] = useState();
+  const [selectMenuValue, setSelectMenuValue] = useState({
+    typeList: [],
+    modelList: [],
+    transmissionList: [],
+  });
+  const router = useRouter();
   let responsiveFontTheme = responsiveFontSizes(theme);
 
-  // TODO: API 조회 후 수정할 dummy data입니다.
-  const itemDummyArr = [
-    {
-      productImgUrl: 'https://source.unsplash.com/random?car',
-      carName: '멋쟁이 자동차1',
-      price: '300',
-      createdDate: '2023-07',
-      distance: '58,972',
-      carWhere: '인천',
-      productUrl: '/products/detail/1',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?bmw',
-      carName: '멋쟁이 자동차2',
-      price: '3000',
-      createdDate: '2023-05',
-      distance: '58,999',
-      carWhere: '대전',
-      productUrl: '/products/detail/2',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?benz',
-      carName: '멋쟁이 자동차3',
-      price: '5000',
-      createdDate: '2023-02',
-      distance: '38,972',
-      carWhere: '부산',
-      productUrl: '/products/detail/3',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?car',
-      carName: '멋쟁이 자동차4',
-      price: '300',
-      createdDate: '2023-07',
-      distance: '58,972',
-      carWhere: '인천',
-      productUrl: '/products/detail/4',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?bmw',
-      carName: '멋쟁이 자동차5',
-      price: '3000',
-      createdDate: '2023-05',
-      distance: '58,999',
-      carWhere: '대전',
-      productUrl: '/products/detail/5',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?benz',
-      carName: '멋쟁이 자동차6',
-      price: '5000',
-      createdDate: '2023-02',
-      distance: '38,972',
-      carWhere: '부산',
-      productUrl: '/products/detail/6',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?car',
-      carName: '멋쟁이 자동차7',
-      price: '300',
-      createdDate: '2023-07',
-      distance: '58,972',
-      carWhere: '인천',
-      productUrl: '/products/detail/7',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?bmw',
-      carName: '멋쟁이 자동차8',
-      price: '3000',
-      createdDate: '2023-05',
-      distance: '58,999',
-      carWhere: '대전',
-      productUrl: '/products/detail/8',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?benz',
-      carName: '멋쟁이 자동차9',
-      price: '5000',
-      createdDate: '2023-02',
-      distance: '38,972',
-      carWhere: '부산',
-      productUrl: '/products/detail/9',
-    },
-    {
-      productImgUrl: 'https://source.unsplash.com/random?benz',
-      carName: '멋쟁이 자동차10',
-      price: '5000',
-      createdDate: '2023-02',
-      distance: '38,972',
-      carWhere: '부산',
-      productUrl: '/products/detail/10',
-    },
-  ];
-
-  // data 불러온 이후 필터링 data에 맞게 렌더링
   useEffect(() => {
+    allProductGetApi().then((data) => {
+      setAllProducts(data);
+    });
     setMounted(true);
   }, []);
 
@@ -130,6 +47,7 @@ export default function Products() {
   };
 
   return (
+    allProducts &&
     mounted && (
       <ThemeProvider theme={responsiveFontTheme}>
         <CssBaseline />
@@ -150,7 +68,9 @@ export default function Products() {
 
             {/* 차량게시글 관련 content */}
             <Container sx={productsCss.contentContainer} maxWidth="md">
-              <ProductCard cardItems={itemDummyArr} />
+              <Grid container spacing={4}>
+                <ProductCard productItems={allProducts.productInfo} />
+              </Grid>
             </Container>
           </Grid>
 
