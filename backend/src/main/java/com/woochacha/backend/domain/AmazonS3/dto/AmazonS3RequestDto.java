4ba838bb134e9f52c48c067067a5fd977149950f
seungyeon gim <kjm061652@naver.com>
2023-09-02 15:31:20 +0900

    feat: 이메일 설정을 위한 Setter 저장

--- backend/src/main/java/com/woochacha/backend/domain/AmazonS3/dto/AmazonS3RequestDto.java
+++ backend/src/main/java/com/woochacha/backend/domain/AmazonS3/dto/AmazonS3RequestDto.java
@@ -1,9 +1,12 @@
 package com.woochacha.backend.domain.AmazonS3.dto;
 
 import lombok.Data;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Setter
 public class AmazonS3RequestDto {
     MultipartFile multipartFile;
     String email;
