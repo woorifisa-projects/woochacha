54d49f0c2758a8c18d1448b8e4b87ac7b1a149c7
seungyeon gim <kjm061652@naver.com>
2023-09-02 12:52:33 +0900

    feat: ApproveSaleController파일에서 /admin/sales/register/:saleFormId API 생성을 위한 PostMapping 작성

--- backend/src/main/java/com/woochacha/backend/domain/car/type/repository/ColorRepository.java
+++ backend/src/main/java/com/woochacha/backend/domain/car/type/repository/ColorRepository.java
@@ -1,7 +1,11 @@
 package com.woochacha.backend.domain.car.type.repository;
 
 import com.woochacha.backend.domain.car.type.entity.Color;
+import com.woochacha.backend.domain.car.type.entity.ColorList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ColorRepository extends JpaRepository<Color, Integer> {
+    Color findByName(ColorList name);
 }
