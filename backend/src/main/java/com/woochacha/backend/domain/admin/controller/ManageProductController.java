d89f858089102f35c9d4a1e7ca03f49a7394807f
seungyeon gim <kjm061652@naver.com>
2023-09-04 14:25:57 +0900

    feat: 매물 관리 리스트 수정 처리를 위한 GetMapping Controller 작성

--- backend/src/main/java/com/woochacha/backend/domain/admin/controller/ManageProductController.java
+++ backend/src/main/java/com/woochacha/backend/domain/admin/controller/ManageProductController.java
@@ -1,6 +1,7 @@
 package com.woochacha.backend.domain.admin.controller;
 
-import com.woochacha.backend.domain.admin.dto.ManageProductFormDto;
+import com.woochacha.backend.domain.admin.dto.magageProduct.EditProductDto;
+import com.woochacha.backend.domain.admin.dto.magageProduct.ManageProductFormDto;
 import com.woochacha.backend.domain.admin.service.impl.ManageProductFormServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,7 +16,7 @@ public class ManageProductController {
     private final ManageProductFormServiceImpl manageProductFormService;
 
     @GetMapping
-    public ResponseEntity<Page<ManageProductFormDto>> getrequestForm(@RequestParam(defaultValue = "0") int page,
+    public ResponseEntity<Page<ManageProductFormDto>> getRequestForm(@RequestParam(defaultValue = "0") int page,
                                                                @RequestParam(defaultValue = "5") int size) {
         Page<ManageProductFormDto> productFormsPage = manageProductFormService.findDeleteEditForm(page, size);
         return ResponseEntity.ok(productFormsPage);
@@ -25,5 +26,11 @@ public class ManageProductController {
     public ResponseEntity<String> permitDeleteRequest(@PathVariable("productId") Long productId){
         manageProductFormService.deleteProduct(productId);
         return ResponseEntity.ok("삭제가 완료되었습니다.");
+    }
+
+    @GetMapping("/edit/{productId}")
+    public ResponseEntity<EditProductDto> getEditForm(@PathVariable("productId") Long productId){
+        EditProductDto editProductDto = manageProductFormService.findEditForm(productId);
+        return ResponseEntity.ok(editProductDto);
     }
 }
