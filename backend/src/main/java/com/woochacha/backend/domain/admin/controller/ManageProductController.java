77b6a8c476c7c5465d0374babb95e5f8a52d2c95
seungyeon gim <kjm061652@naver.com>
2023-09-04 11:36:02 +0900

    feat: 매물 관리 리스트 GET요청을 위한 Controller 작성, postman 테스트를 위한 SecurityConfig파일 수정

--- backend/src/main/java/com/woochacha/backend/domain/admin/controller/ManageProductController.java
+++ backend/src/main/java/com/woochacha/backend/domain/admin/controller/ManageProductController.java
@@ -1,16 +1,23 @@
 package com.woochacha.backend.domain.admin.controller;
 
+import com.woochacha.backend.domain.admin.dto.ManageProductFormDto;
+import com.woochacha.backend.domain.admin.service.impl.ManageProductFormServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/product")
 public class ManageProductController {
 
+    private final ManageProductFormServiceImpl manageProductFormService;
 
-
+    @GetMapping
+    public ResponseEntity<Page<ManageProductFormDto>> getrequestForm(@RequestParam(defaultValue = "0") int page,
+                                                               @RequestParam(defaultValue = "5") int size) {
+        Page<ManageProductFormDto> productFormsPage = manageProductFormService.findDeleteEditForm(page, size);
+        return ResponseEntity.ok(productFormsPage);
+    }
 }
