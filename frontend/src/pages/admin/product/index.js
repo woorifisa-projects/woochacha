c3a5d1ea4c254e5fba2a8fa81d4f0992eb76b2c2
alsth712 <alsth712@naver.com>
2023-09-04 19:57:43 +0900

    chore: 매물관리, 거래관리 페이지 수정

--- frontend/src/pages/admin/product/index.js
+++ frontend/src/pages/admin/product/index.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Box, CardMedia, Grid, Stack, Typography } from '@mui/material';
 import AdminPageLayout from '@/layouts/admin/AdminPageLayout';
-import { Typography } from '@mui/material';
 import withAdminAuth from '@/hooks/withAdminAuth';
 import BasicModal from '@/components/common/BasicModal';
-import { DELETE_MODAL } from '@/constants/string';
-import { useRouter } from 'next/router';
 import BasicButtonTable from '@/components/admin/ProductTable';
+import { ADMIN_EDIT_MODAL, DELETE_MODAL } from '@/constants/string';
 
 function AdminProductList() {
   const [mounted, setMounted] = useState(false);
@@ -14,9 +14,10 @@ function AdminProductList() {
   // Modal 버튼 클릭 유무
   const [showModal, setShowModal] = useState(false);
   const handleClickModal = () => setShowModal(!showModal);
+  const [editFlag, setEditFlag] = useState();
 
   /**
-   * 수정 form 제출
+   * 삭제
    */
   const handleDelete = async () => {
     try {
@@ -28,30 +29,71 @@ function AdminProductList() {
     }
   };
 
-  const mypageCss = {
-    mypageTitle: {
+  /**
+   * 수정
+   */
+  const handleEdit = async () => {
+    try {
+      // await adminEditProduct(memberId, productId, updateData);
+      alert('수정 완료');
+      router.push(`/admin/product`);
+    } catch (error) {
+      console.log('실패');
+    }
+  };
+
+  // TODO: DUMMY_DATA - edit 요청 시, get해와서 보여주는 data
+  const dummyData = {
+    title: '기아 모닝 2010년형',
+    imageURL: 'https://woochacha.s3.ap-northeast-2.amazonaws.com/product/22%EB%82%982222/1',
+    price: 2690,
+    updatePrice: 3200,
+  };
+
+  const adminProductCss = {
+    mainTitle: {
       my: 10,
       color: '#1490ef',
       fontWeight: 'bold',
+    },
+    modalTitle: {
+      fontWeight: 'bold',
+      mb: 3,
+    },
+    modalCardMedia: {
+      width: '15rem',
+      height: '15rem',
+      borderRadius: '1rem',
+    },
+    modalGridBox: {
+      mb: 7,
+    },
+    modalPriceBox: {
+      display: 'flex',
+      px: 5,
+      pl: 5,
+      pr: 5,
+      flexDirection: 'column',
+      justifyContent: 'center',
     },
   };
 
   const table_cell_data = [
     {
       headerLabel: '게시글',
-      contentCell: 'id',
+      contentCell: 'title',
     },
     {
       headerLabel: '판매자',
-      contentCell: 'seller',
+      contentCell: 'sellerName',
     },
     {
-      headerLabel: '상태',
-      contentCell: 'status',
+      headerLabel: '판매자 이메일',
+      contentCell: 'sellerEmail',
     },
     {
       headerLabel: '관리',
-      contentCell: 'isAvailable',
+      contentCell: 'manageType',
     },
   ];
 
@@ -59,22 +101,25 @@ function AdminProductList() {
   const dummy_content_data = {
     content: [
       {
-        id: '1',
-        seller: '홍길길',
-        status: '김김김',
-        isAvailable: 0,
+        productId: '14',
+        title: '기아 올 뉴 카니발 2018년형',
+        sellerName: '홍길길',
+        sellerEmail: 'fisafisa@naver.com',
+        manageType: 0,
       },
       {
-        id: '2',
-        seller: '홍김김',
-        status: '김김김',
-        isAvailable: 1,
+        productId: '2',
+        title: '기아 올 뉴 카니발 2018년형',
+        sellerName: '홍길길',
+        sellerEmail: 'fisafisa@naver.com',
+        manageType: 0,
       },
       {
-        id: '3',
-        seller: '왕길길',
-        status: '김김김',
-        isAvailable: 0,
+        productId: '3',
+        title: '기아 올 뉴 카니발 2018년형',
+        sellerName: '홍길길',
+        sellerEmail: 'fisafisa@naver.com',
+        manageType: 1,
       },
     ],
   };
@@ -87,22 +132,59 @@ function AdminProductList() {
   return (
     mounted && (
       <>
-        <Typography sx={mypageCss.mypageTitle} component="h4" variant="h4" gutterBottom>
+        <Typography sx={adminProductCss.mainTitle} component="h4" variant="h4" gutterBottom>
           관리자 페이지 - 매물 관리 리스트
         </Typography>
         <BasicButtonTable
           headerData={table_cell_data}
           contentData={dummy_content_data.content}
-          moveUrl={`/admin/product/edit/`}
-          callbackFunc={handleClickModal}
+          deleteFunc={handleClickModal}
+          editFunc={handleClickModal}
+          setEditFlag={setEditFlag}
+          moveUrl={`/product/detail/`}
         />
         {showModal && (
           <BasicModal
             onClickModal={handleClickModal}
             isOpen={showModal}
-            modalContent={DELETE_MODAL.CONTENTS}
-            callBackFunc={handleDelete}
-          />
+            modalContent={editFlag ? ADMIN_EDIT_MODAL.CONTENTS : DELETE_MODAL.CONTENTS}
+            callBackFunc={editFlag ? handleEdit : handleDelete}>
+            {/* modal 자식 요소 - edit 의 경우, 가격 승인을 띄워줘야함 */}
+            {editFlag ? (
+              <Grid container spacing={2} noValidate justifyContent="center" alignItems="center">
+                <Grid item xs={12} md={6}>
+                  <Typography variant="h5" sx={adminProductCss.modalTitle}>
+                    {dummyData.title}
+                  </Typography>
+                  <Stack direction="column" alignItems="center" spacing={2} mb={5}>
+                    <CardMedia
+                      sx={adminProductCss.modalCardMedia}
+                      image={dummyData.imageURL}
+                      title="게시글 이미지"
+                    />
+                  </Stack>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box sx={adminProductCss.modalPriceBox}>
+                    <Grid sx={adminProductCss.modalGridBox}>
+                      <Typography variant="h6" component="h6" textAlign="left" fontWeight="bold">
+                        현재 가격
+                      </Typography>
+                      <Typography textAlign="left">{`${dummyData.price} 만원`}</Typography>
+                    </Grid>
+                    <Grid>
+                      <Typography variant="h6" component="h6" textAlign="left" fontWeight="bold">
+                        수정 신청한 가격
+                      </Typography>
+                      <Typography textAlign="left">{`${dummyData.updatePrice} 만원`}</Typography>
+                    </Grid>
+                  </Box>
+                </Grid>
+              </Grid>
+            ) : (
+              ''
+            )}
+          </BasicModal>
         )}
       </>
     )
