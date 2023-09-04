a0630b373ab800879b61ed7c058d8f180b2eeb49
alsth712 <alsth712@naver.com>
2023-09-04 19:58:40 +0900

    chore: 이미지 슬라이더 수정

--- frontend/src/components/product/ImageSlider.js
+++ frontend/src/components/product/ImageSlider.js
@@ -1,32 +1,15 @@
 import { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
-import { Box, MobileStepper, Paper, Typography, Button } from '@mui/material';
+import { Box, MobileStepper, Button } from '@mui/material';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-// DUMMY-DATA
-const steps = [
-  {
-    label: '전면부',
-    imageUrl:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: '측면부',
-    imageUrl:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: '후면부',
-    imageUrl: `https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250`,
-  },
-];
-
 export default function ImageSlider(props) {
-  const { photos } = props;
+  const { image } = props;
+  console.log(image);
   const theme = useTheme();
   const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = steps.length;
+  const maxSteps = image.length;
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -36,41 +19,31 @@ export default function ImageSlider(props) {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const imageSliderCss = {
+    box: {
+      mt: 3,
+      height: '20rem',
+      display: 'block',
+      maxWidth: '20rem',
+      minWidth: '20rem',
+      overflow: 'hidden',
+      width: '100%',
+      borderRadius: '15px',
+    },
+    mobileStepper: {
+      display: 'flex',
+      alignItems: 'center',
+      height: 50,
+      pl: 2,
+      bgcolor: 'rgb(255,255,255,0.3)',
+    },
+  };
+
   return (
     <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'rgb(255,255,255,0.3)',
-        }}>
-        <Typography>{steps[activeStep].label}</Typography>
-      </Paper>
-
-      <Box
-        component="img"
-        sx={{
-          height: 255,
-          display: 'block',
-          maxWidth: 400,
-          overflow: 'hidden',
-          width: '100%',
-        }}
-        src={steps[activeStep].imageUrl}
-        alt={steps[activeStep].label}
-      />
+      <Box component="img" sx={imageSliderCss.box} src={image[activeStep]} />
       <MobileStepper
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'rgb(255,255,255,0.3)',
-        }}
+        sx={imageSliderCss.mobileStepper}
         variant="text"
         steps={maxSteps}
         position="static"
