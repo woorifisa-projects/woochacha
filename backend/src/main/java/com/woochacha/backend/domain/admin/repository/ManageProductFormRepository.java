7ae8744afe15802367b71911fd043c201820905d
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2023-09-04 16:18:07 +0900

    [Feat] 관리자 매물관리 기능 구현 (#149)
    * 충돌해결
    
    * feat: 매물 관리 리스트 GET요청을 위한 Dto 객체 생성
    
    * feat: 매물 관리 리스트 GET요청을 위한 Dto 객체 수정 (피그마 화면 참고)
    
    * feat: 매물 관리 리스트 GET요청을 위한 service, repository 생성
    
    * feat: 매물 관리 리스트 GET요청을 위한 Controller 작성, postman 테스트를 위한 SecurityConfig파일 수정
    
    * feat: 매물 관리 리스트 삭제 승인 PatcMapping을 위한 service, repository 작성
    
    * feat: 매물 관리 리스트 삭제 승인 PatcMapping을 위한 Controller 작성
    
    * feat: 매물 관리 리스트 삭제 승인 PatcMapping을 위한 Controller 경로 수정
    
    * feat: 매물 관리 리스트 수정 처리를 위한 데이터 조회 Dto 작성
    
    * feat: 매물 관리 리스트 수정 처리를 위한 데이터 조회 Dto 작성
    
    * feat: 매물 관리 리스트 수정 처리를 위한 GET요청 service, repository 작성
    
    * feat: 매물 관리 리스트 수정 처리를 위한 GetMapping Controller 작성
    
    * feat: 매물 관리 리스트 수정 반려, 승인을 위한 로직 작성
    
    * feat: 매물 관리 리스트 수정 반려, 승인을 위한 두 개의 PatchMapping 작성
    
    * chore: 주석 추가
    
    * chore: 리뷰 반영
    
    * chore: 리뷰 반영

---
+++ backend/src/main/java/com/woochacha/backend/domain/admin/repository/ManageProductFormRepository.java
@@ -0,0 +1,50 @@
+package com.woochacha.backend.domain.admin.repository;
+
+import com.woochacha.backend.domain.admin.dto.magageProduct.EditProductDto;
+import com.woochacha.backend.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManageProductFormRepository extends JpaRepository<Product, Long> {
+
+    // 매물 관리 리스트 조회
+    @Query("SELECT p.id, CONCAT(CAST(cd.model.name AS string), ' ', cd.carName.name, ' ', CAST(cd.year AS string), '년형') AS title, " +
+            "m.name, m.phone, p.status.id " +
+            "FROM Product p " +
+            "JOIN p.carDetail cd " +
+            "JOIN p.saleForm sf " +
+            "JOIN sf.member m " +
+            "WHERE p.status.id IN (6, 9) " +
+            "ORDER BY p.updatedAt ASC ")
+    Page<Object[]> getDeleteEditForm(Pageable pageable);
+
+    // 매물 삭제 신청 처리
+    @Modifying
+    @Query("UPDATE Product p SET p.status = 7 WHERE p.id = :productId")
+    void deleteProduct(@Param("productId") Long productId);
+
+    // 매물 가격 수정 처리
+    @Query("SELECT new com.woochacha.backend.domain.admin.dto.magageProduct.EditProductDto(CONCAT(CAST(cd.model.name AS string), ' ', cd.carName.name, ' ', CAST(cd.year AS string), '년형'), ci.imageUrl, p.price, p.updatePrice) " +
+            "FROM Product p " +
+            "JOIN p.carDetail cd " +
+            "JOIN p.carImages ci " +
+            "WHERE p.id = :productId " +
+            "AND ci.imageUrl LIKE '%/1' ")
+    EditProductDto getEditForm(@Param("productId") Long productId);
+
+    // 매물 가격 수정 반려
+    @Modifying
+    @Query("UPDATE Product p SET p.status = 4 WHERE p.id = :productId")
+    void denyEditRequest(@Param("productId") Long productId);
+
+    // 매물 가격 수정 승인
+    @Modifying
+    @Query("UPDATE Product p SET p.status = 4, p.price = p.updatePrice WHERE p.id = :productId")
+    void permitEditRequest(@Param("productId") Long productId);
+}
