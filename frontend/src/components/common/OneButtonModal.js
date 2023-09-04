b85c02ece8efaf9fd386f84dbfb0f0575c5afa39
alsth712 <alsth712@naver.com>
2023-09-04 19:56:50 +0900

    feat: 버튼 1개 모달창 추가

---
+++ frontend/src/components/common/OneButtonModal.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+
+export default function OneButtonModal(props) {
+  const [mounted, setMounted] = useState(false);
+  const { onClickModal, isOpen, modalContent, callBackFunc, children } = props;
+
+  const handleClickNo = () => {
+    onClickModal(false);
+  };
+
+  const handleClickYes = () => {
+    onClickModal(false);
+    callBackFunc(); // 콜백함수 실행
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    mounted && (
+      <div>
+        <Dialog
+          maxWidth="xs"
+          fullWidth
+          open={isOpen}
+          onClose={handleClickNo}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">{modalContent.modalTitle}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {modalContent.modalContents}
+            </DialogContentText>
+            {children}
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" onClick={handleClickYes} color="primary">
+              {modalContent.modalYesBtn}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  );
+}
