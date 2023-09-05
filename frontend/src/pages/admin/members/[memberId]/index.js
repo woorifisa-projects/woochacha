73dcc7088324d24896239716d9b8b034e0a558a0
ShinChanCoding <juju6050@naver.com>
2023-09-05 14:26:14 +0900

    design : 사용자 정보 상세 페이지 구현

--- frontend/src/pages/admin/members/[memberId]/index.js
+++ frontend/src/pages/admin/members/[memberId]/index.js
@@ -15,36 +15,50 @@ function AdminUserDetail() {
       fontWeight: 'bold',
     },
     card: {
-      maxWidth: '50rem',
-      maxHeight: '45rem',
+      marginTop: '25px',
+      maxWidth: '100%',
+      maxHeight: '100%',
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       marginBottom: 10,
-      paddingY: 8,
+      paddingY: 1,
       textAlign: 'center',
       boxShadow: 7,
       borderRadius: '3rem',
     },
     cardMedia: {
-      width: '180px',
-      height: '180px',
+      marginTop: 2,
+      marginBottom: 2,
+      width: '220px',
+      height: '220px',
       borderRadius: '50%',
     },
     profileTitle: { color: '#1490ef' },
-    gridContainer: { marginTop: 5, width: 'fit-content', marginBottom: 5 },
+    gridContainer: {
+      textAlign: 'left',
+      marginTop: 5,
+      width: 'fit-content',
+      marginBottom: 5,
+      paddingLeft: 3,
+    },
     buttonCard: {
       marginBottom: 5,
     },
     itemTitle: {
+      color: '#1490ef',
+      fontWeight: 'bold',
+      borderBottom: '1px solid #1490ef',
+    },
+    itemContent: {
       fontWeight: 'bold',
     },
   };
 
   const dummyData = {
     profileImage: 'https://woochacha.s3.ap-northeast-2.amazonaws.com/profile/user1%40woorifisa.com',
-    name: '홍길동',
+    name: '이소민',
     phone: '01022223333',
     email: 'user@woorifisa.com',
   };
@@ -63,9 +77,6 @@ function AdminUserDetail() {
   return (
     mounted && (
       <>
-        <Typography sx={adminUserProfileCss.mypageTitle} component="h4" variant="h4" gutterBottom>
-          관리자 페이지 - 유저 상세조회
-        </Typography>
         <Card sx={adminUserProfileCss.card}>
           <CardMedia
             sx={adminUserProfileCss.cardMedia}
@@ -78,20 +89,77 @@ function AdminUserDetail() {
               variant="h4"
               component="div"
               sx={adminUserProfileCss.profileTitle}>
-              {dummyData.name} 님
+              {dummyData.name}
             </Typography>
             <Grid container spacing={2} sx={adminUserProfileCss.gridContainer}>
-              <Grid item xs={6} sx={adminUserProfileCss.itemTitle}>
-                전화번호
+              <Grid item xs={12} sm={6}>
+                <Grid container spacing={2} ml={0.5}>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemTitle}>
+                    회원 정보
+                  </Grid>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemTitle}></Grid>
+                </Grid>
+
+                <Grid container spacing={2} sx={adminUserProfileCss.gridContainer}>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemContent}>
+                    상태
+                  </Grid>
+                  <Grid item xs={8}>
+                    정상
+                  </Grid>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemContent}>
+                    전화번호
+                  </Grid>
+                  <Grid item xs={8}>
+                    {dummyData.phone}
+                  </Grid>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemContent}>
+                    이메일
+                  </Grid>
+                  <Grid item xs={8}>
+                    {dummyData.email}
+                  </Grid>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemContent}>
+                    가입일
+                  </Grid>
+                  <Grid item xs={8}>
+                    2023년 12월 10일
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item xs={6}>
-                {dummyData.phone}
-              </Grid>
-              <Grid item xs={6} sx={adminUserProfileCss.itemTitle}>
-                이메일
-              </Grid>
-              <Grid item xs={6}>
-                {dummyData.email}
+              <Grid item xs={12} sm={6}>
+                <Grid container spacing={2} ml={0.5}>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemTitle}>
+                    회원 정보
+                  </Grid>
+                  <Grid item xs={4} sx={adminUserProfileCss.itemTitle}></Grid>
+                </Grid>
+                <Grid container spacing={2} sx={adminUserProfileCss.gridContainer}>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemContent}>
+                    판매 중
+                  </Grid>
+                  <Grid item xs={6}>
+                    3건
+                  </Grid>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemContent}>
+                    판매 완료
+                  </Grid>
+                  <Grid item xs={6}>
+                    4건
+                  </Grid>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemContent}>
+                    구매 대기
+                  </Grid>
+                  <Grid item xs={6}>
+                    2건
+                  </Grid>
+                  <Grid item xs={6} sx={adminUserProfileCss.itemContent}>
+                    구매 완료
+                  </Grid>
+                  <Grid item xs={6}>
+                    1건
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </CardContent>
@@ -100,7 +168,10 @@ function AdminUserDetail() {
               variant="contained"
               fullWidth
               onClick={() => handleMove(`/admin/members/edit/${userId}`)}>
-              수정하기
+              수정
+            </Button>
+            <Button variant="contained" color="error">
+              삭제
             </Button>
           </CardActions>
         </Card>
