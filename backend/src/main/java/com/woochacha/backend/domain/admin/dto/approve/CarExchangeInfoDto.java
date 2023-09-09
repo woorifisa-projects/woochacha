749b9884f2ffd2a8afdc1a13b4f46cb3bf7980a2
KimSuhyun <93786956+ooutta@users.noreply.github.com>
2023-09-09 21:02:59 +0900

    [Chore] QLDB exchange info 필드명 수정&프론트엔드 리다이렉트 경로 수정 (#223)
    * test: dummy 데이터 넣기 전 커밋
    
    * chore: qldb car exchange 필드명 수정, front 페이지 이동 경로명 수정
    
    * Delete backend/src/test/java/com/woochacha/backend/domain/member/controller/MemberControllerTest.java
    
    * Delete backend/src/test/java/com/woochacha/backend/domain/product/controller/ProductControllerTest.java

--- backend/src/main/java/com/woochacha/backend/domain/admin/dto/approve/CarExchangeInfoDto.java
+++ backend/src/main/java/com/woochacha/backend/domain/admin/dto/approve/CarExchangeInfoDto.java
@@ -1,10 +1,14 @@
 package com.woochacha.backend.domain.admin.dto.approve;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarExchangeInfoDto {
     // 교체이력 dto
     private String exchangeType;
