5682227e4dff43b98ce04ef46a7c76addf1796bf
Somin Lee <81960250+purin48@users.noreply.github.com>
2023-09-09 15:29:18 +0900

    [feature] 상품 필터링 API 기능 연동 (#215)
    * remove: 필요없는 파일 제거
    
    * style: multiple select css 수정
    
    * feat: filtering 관련 기능 추가 및 api 연동
    
    * feat: filtering 관련 api 연동 및 주석 추가
    
    * feat: main page filtering, serch bar api 연동 및 수정

--- frontend/src/pages/product/index.js
+++ frontend/src/pages/product/index.js
@@ -1,5 +1,6 @@
 import {
-  Container,
+  Button,
+  Card,
   CssBaseline,
   Grid,
   Pagination,
@@ -11,23 +12,39 @@ import theme from '@/styles/theme';
 import MiniCard from '@/components/common/MiniCard';
 import SearchBar from '@/components/product/SearchBar';
 import ProductCard from '@/components/product/ProductCard';
-import FilterSideBar from '@/components/product/FilterSideBar';
 import { useEffect, useState } from 'react';
-import { allProductGetApi, keywordProductGetApi } from '@/services/productApi';
-import { useRouter } from 'next/router';
+import {
+  allProductGetApi,
+  filteringProductGetApi,
+  keywordProductGetApi,
+} from '@/services/productApi';
+import MultipleSelect from '@/components/product/MultipleSelect';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Products() {
   const [mounted, setMounted] = useState(false);
   const [allProducts, setAllProducts] = useState();
   const [itemsPerPage, setItemsPerPage] = useState(10);
-  const [selectMenuValue, setSelectMenuValue] = useState({
-    typeList: [],
-    modelList: [],
-    transmissionList: [],
-  });
-  const router = useRouter();
+  const [selectMenus, setSelectMenus] = useState();
+  const [selectMenuValue, setSelectMenuValue] = useState({});
   let responsiveFontTheme = responsiveFontSizes(theme);
 
+  /**
+   * selectmenu의 키와 라벨을 매핑
+   */
+  const selectBoxLabelMap = {
+    typeList: '차종',
+    modelList: '모델',
+    carNameList: '차 이름',
+    fuelList: '연료',
+    colorList: '색상',
+    transmissionList: '변속기',
+    branchList: '차고지',
+  };
+
+  /**
+   * 첫 렌더링시, 전체 product get
+   */
   useEffect(() => {
     allProductGetApi().then((data) => {
       setAllProducts(data);
@@ -35,15 +52,56 @@ export default function Products() {
     setMounted(true);
   }, []);
 
-  // 검색어를 받아와서 API 호출 후 결과를 상태로 설정하는 함수
+  /**
+   * allProducts가 업데이트될 때만 selectBoxes를 생성하도록 이동
+   */
+  useEffect(() => {
+    if (allProducts) {
+      const selectBoxes = Object.keys(allProducts.productFilterInfo).map((key) => ({
+        id: key,
+        label: selectBoxLabelMap[key] || key,
+      }));
+      setSelectMenus(selectBoxes);
+    }
+  }, [allProducts]);
+
+  /**
+   * 검색어를 받아와서 API 호출 후 결과를 상태로 설정하는 함수
+   * */
   const handleSearch = (keyword) => {
-    console.log(keyword)
     keywordProductGetApi(keyword).then((data) => {
-      console.log(data);
       setAllProducts({
         productInfo: data,
-      })
+      });
     });
+  };
+
+  /**
+   * 필터링 관련 함수
+   */
+  const handleFiltering = () => {
+    filteringProductGetApi(selectMenuValue).then((data) => {
+      setAllProducts({
+        ...allProducts,
+        productInfo: data,
+      });
+    });
+  };
+
+  /**
+   * selectbox 선택 시, 선택한 selectItem 저장 함수
+   */
+  const handleChangeSelect = (e, selectId) => {
+    const valueArr = e.target.value;
+    const updatedValue = {
+      ...selectMenuValue,
+      [selectId]: valueArr.map((item) => {
+        return {
+          id: item,
+        };
+      }),
+    };
+    setSelectMenuValue(updatedValue);
   };
 
   const productsCss = {
@@ -55,10 +113,24 @@ export default function Products() {
     contentContainer: {
       py: 3,
     },
+    filteringBox: {
+      maxWidth: '100%',
+    },
+    filteringBtn: {
+      float: 'right',
+      marginTop: 3,
+    },
+    card: {
+      padding: 2,
+      width: '100%',
+    },
+    cardTypo: {
+      display: 'flex',
+      fontWeight: 'bold',
+      my: 3,
+    },
     pagination: { display: 'flex', justifyContent: 'center', my: 8 },
   };
-
-
 
   return (
     allProducts &&
@@ -68,29 +140,56 @@ export default function Products() {
         {/* main page */}
         <main>
           {/* 페이지 상단 serch box */}
-          <MiniCard colorVal="#def2ff" shadowVal={3} marginVal={10}>
-            <Typography gutterBottom variant="h5" component="h5" mb={3}>
-              궁금한 차량 조회하기
-            </Typography>
-            <SearchBar onSearch={handleSearch} />
-          </MiniCard>
+          <Grid mx="auto" container maxWidth="xl">
+            <MiniCard colorVal="#def2ff" shadowVal={3} marginVal={10}>
+              <Typography gutterBottom variant="h5" component="h5" mb={3}>
+                궁금한 차량 조회하기
+              </Typography>
+              <SearchBar onSearch={handleSearch} />
+            </MiniCard>
+          </Grid>
 
           {/* 메인 페이지 content */}
-          <Grid sx={productsCss.gridContent}>
+          <Grid mx="auto" container sx={productsCss.gridContent} maxWidth="xl">
             {/* 구매 페이지 filltering side bar */}
-            <FilterSideBar />
+            <Grid item mx={4} xs={2} spacing={1} alignItems="center" justifyContent="center" mb={8}>
+              <Card sx={productsCss.card}>
+                <Typography variant="body1" sx={productsCss.cardTypo}>
+                  우차차에서 내 차 찾기
+                  <SearchIcon color="primary" fontSize="large" />
+                </Typography>
+                {selectMenus &&
+                  selectMenus.map((item, idx) => {
+                    return (
+                      <Grid key={idx} item sx={productsCss.filteringBox}>
+                        <MultipleSelect
+                          sx={productsCss.filteringBox}
+                          key={idx}
+                          selectMenu={item}
+                          selectItems={allProducts.productFilterInfo[`${item.id}`]}
+                          onChangeSelect={handleChangeSelect}
+                        />
+                      </Grid>
+                    );
+                  })}
+
+                <Button sx={productsCss.filteringBtn} variant="contained" onClick={handleFiltering}>
+                  필터링 검색
+                </Button>
+              </Card>
+            </Grid>
 
             {/* 차량게시글 관련 content */}
-            <Container sx={productsCss.contentContainer} maxWidth="md">
+            <Grid item lg={8} mx={8} sx={productsCss.contentContainer} maxWidth="lg">
               <Grid container spacing={4}>
                 <ProductCard productItems={allProducts.productInfo} />
               </Grid>
-            </Container>
+            </Grid>
           </Grid>
 
           {/* pagination */}
           <Grid sx={productsCss.pagination}>
-            <Pagination count={(Math.ceil(allProducts.productInfo.length / itemsPerPage)) || 1} />
+            <Pagination count={Math.ceil(allProducts.productInfo.length / itemsPerPage) || 1} />
           </Grid>
         </main>
       </ThemeProvider>
--- frontend/src/services/productApi.js
+++ frontend/src/services/productApi.js
@@ -1,5 +1,8 @@
 import { authInstance, jsonInstance } from '@/utils/api';
 
+/**
+ * [상품] 모든 상품을 조회 (GET)
+ */
 export const allProductGetApi = async () => {
   try {
     const response = await jsonInstance.get('/product');
@@ -11,6 +14,9 @@ export const allProductGetApi = async () => {
   }
 };
 
+/**
+ * [상품] 구매 요청 (PATCH)
+ */
 export const submitEditRegistered = async (memberId, productId, editedData) => {
   try {
     const response = await jsonInstance.patch(`/mypage/registered/edit`, {
@@ -30,6 +36,9 @@ export const submitEditRegistered = async (memberId, productId, editedData) => {
   }
 };
 
+/**
+ * [상품] 상품 필터링 (POST)
+ */
 export const filteringProductGetApi = async (filteredItem) => {
   console.log(filteredItem);
   try {
@@ -42,6 +51,9 @@ export const filteringProductGetApi = async (filteredItem) => {
   }
 };
 
+/**
+ * [상품] 특정 상품의 상세 정보 조회 (GET)
+ */
 export const productDetailGetApi = async (productId) => {
   try {
     const response = await jsonInstance.get(`/product/${productId}`);
@@ -53,6 +65,9 @@ export const productDetailGetApi = async (productId) => {
   }
 };
 
+/**
+ * [상품] 특정 상품의 구매 요청 (POST)
+ */
 export const purchaseRequest = async (purchaseForm) => {
   // userId,
   try {
@@ -65,6 +80,9 @@ export const purchaseRequest = async (purchaseForm) => {
   }
 };
 
+/**
+ * [상품] 전체 상품의 키워드 조회 (GET)
+ */
 export const keywordProductGetApi = async (keyword) => {
   console.log(keyword);
   try {
@@ -79,6 +97,9 @@ export const keywordProductGetApi = async (keyword) => {
   }
 };
 
+/**
+ * [판매] 판매 폼 조회 시, 차고지 정보 불러오기 (GET)
+ */
 export const getBranchApi = async () => {
   try {
     const response = await authInstance.get('/products/sale');
@@ -90,6 +111,9 @@ export const getBranchApi = async () => {
   }
 };
 
+/**
+ * [판매] 판매 폼 요청 (POST)
+ */
 export const saleFormRequest = async (saleForm) => {
   try {
     const response = await authInstance.post('/products/sale', saleForm);
