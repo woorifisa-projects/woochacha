374d0cffee22eda6b9957fdfc846448bc84c9e06
KimSuhyun <suhun1054@gmail.com>
2023-09-11 15:02:10 +0900

    test: mypage 테스트 코드 작성, Rest Docs 적용, Product&Member Rest Docs 수정, MyPage 컨트롤러 리팩토링, MyPage Service 선언부 작성

--- backend/src/main/java/com/woochacha/backend/domain/mypage/dto/UpdatePriceDto.java
+++ backend/src/main/java/com/woochacha/backend/domain/mypage/dto/UpdatePriceDto.java
@@ -1,15 +1,23 @@
 package com.woochacha.backend.domain.mypage.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-@Data
+@Getter
+@Setter
 public class UpdatePriceDto {
 
     // 유효성 처리를 위해 Dto객체 생성
     @NotNull(message = "필수 입력 값입니다.")
     @Min(value = 0, message = "가격은 0보다 큰 값이어야 합니다.")
     private Integer updatePrice; // 수정할 가격
+
+    public UpdatePriceDto() {}
+
+    public UpdatePriceDto(Integer updatePrice) {
+        this.updatePrice = updatePrice;
+    }
 }
--- backend/src/main/java/com/woochacha/backend/domain/mypage/service/MypageService.java
+++ backend/src/main/java/com/woochacha/backend/domain/mypage/service/MypageService.java
@@ -1,4 +1,30 @@
 package com.woochacha.backend.domain.mypage.service;
 
+import com.woochacha.backend.domain.AmazonS3.dto.AmazonS3RequestDto;
+import com.woochacha.backend.domain.mypage.dto.*;
+import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+
 public interface MypageService {
+    Page<ProductResponseDto> getRegisteredProductsByMemberId(Long memberId, int pageNumber, int pageSize);
+
+    Page<ProductResponseDto> getSoldProductsByMemberId(Long memberId, int pageNumber, int pageSize);
+
+    Page<ProductResponseDto> getPurchaseProductsByMemberId(Long memberId, int pageNumber, int pageSize);
+
+    ProfileDto getProfileByMemberId(Long memberId);
+
+    Page<SaleFormDto> getSaleFormsByMemberId(Long memberId, int pageNumber, int pageSize);
+
+    Page<ProductResponseDto> getPurchaseRequestByMemberId(Long memberId, int pageNumber, int pageSize);
+
+    EditProductDto getProductEditRequestInfo(Long memberId, Long productId);
+    String editProductEditRequestInfo(Long memberId, Long productId, UpdatePriceDto updatePriceDto);
+
+    String productDeleteRequest(Long productId, Long memberId);
+
+    EditProfileDto getProfileForEdit(Long memberId);
+
+    String editProfile(Long memberId, AmazonS3RequestDto amazonS3RequestDto) throws IOException;
 }
