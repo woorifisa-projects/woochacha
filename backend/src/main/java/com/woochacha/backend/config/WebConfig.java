85f4eddd89e14e1b42c96620110fbb7227938b42
KimSuhyun <93786956+ooutta@users.noreply.github.com>
2023-09-11 20:07:19 +0900

    [Feature] product get all 페이지네이션 적용 (#253)
    * feat: product get all 페이지네이션 적용
    
    * chore: 불필요한 webConfig 제거
    
    * chore: test code에 pageable 추가

--- backend/src/main/java/com/woochacha/backend/config/WebConfig.java
+++ backend/src/main/java/com/woochacha/backend/config/WebConfig.java
@@ -3,17 +3,14 @@ package com.woochacha.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8080", "http://localhost:80", "https://web.postman.co/",
-//                        "http://15.165.239.79:80", "http://localhost:8080",
-//                        "https://15.165.239.79:80", "https://localhost:8080") // 허용할 출처
                 .allowedMethods("GET", "POST", "PATCH", "OPTIONS", "PUT", "DELETE", "HEAD") // 허용할 HTTP method
                 .allowedOrigins("access-control-allow-origin")
-//                .exposedHeaders("Authorization")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
     }
--- backend/src/main/java/com/woochacha/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/woochacha/backend/domain/product/service/ProductService.java
@@ -5,11 +5,12 @@ import com.woochacha.backend.domain.product.dto.ProductDetailResponseDto;
 import com.woochacha.backend.domain.product.dto.ProductPurchaseRequestDto;
 import com.woochacha.backend.domain.product.dto.all.ProductInfo;
 import com.woochacha.backend.domain.product.dto.filter.ProductFilterInfo;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ProductService {
-    ProductAllResponseDto findAllProduct();
+    ProductAllResponseDto findAllProduct(Pageable pageable);
     ProductDetailResponseDto findDetailProduct(Long productId);
     List<ProductInfo> findFilteredProduct(ProductFilterInfo productFilterInfo);
     void applyPurchaseForm(ProductPurchaseRequestDto productPurchaseRequestDto);
