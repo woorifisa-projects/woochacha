1e0aee9890d7799cdc2548f8d054f110fd866d6c
ShinChanCoding <juju6050@naver.com>
2023-09-14 11:48:53 +0900

    chore : 메세지를 보내기 위한 Dto 분리

--- backend/src/main/java/com/woochacha/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/woochacha/backend/domain/member/controller/MemberController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
-public class MemberController extends Exception{
+public class MemberController{
 
     private final SignService signService;
 
