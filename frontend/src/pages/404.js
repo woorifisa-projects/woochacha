0a5592e828a9dc42a515bda9567ef020b1f251b2
alsth712 <alsth712@naver.com>
2023-09-17 12:40:11 +0900

    style:404 페이지 추가

---
+++ frontend/src/pages/404.js
@@ -0,0 +1,15 @@
+import { Stack } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import image404 from '../../public/assets/images/woochacha-404.svg';
+import styles from './404.module.css';
+
+export default function Custom404() {
+  return (
+    <div className={styles.box}>
+      <Stack alignItems="center" className={styles.bannerImageContainer}>
+        <Image src={image404} layout="fill" className={styles.bannerImage} />
+      </Stack>
+    </div>
+  );
+}
